97d60a483780fefc5567a19221707005fd5a9c20
Kevin Li <kevinli042389@gmail.com>
2018-12-21 11:43:47 +0800

    Update rake.py

--- rake-extraction-algorithm/rake.py
+++ rake-extraction-algorithm/rake.py
@@ -1,9 +1,4 @@
 # Implementation of RAKE - Rapid Automtic Keyword Exraction algorithm
-# as described in:
-# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010). 
-# Automatic keyword extraction from indi-vidual documents. 
-# In M. W. Berry and J. Kogan (Eds.), Text Mining: Applications and Theory.unknown: John Wiley and Sons, Ltd.
-
 import re
 import operator
 
